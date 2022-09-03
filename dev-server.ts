f6fc89de7c993badcdfd89967690c5126b05044f
thathsara404 <thathsararaviraj@gmail.com>
2022-09-03 09:39:26 +0530

    add husky for pre-commit hooks, add local url to server log, add env-cmd environments

--- dev-server.ts
+++ dev-server.ts
@@ -2,16 +2,19 @@
 
 import App from './app';
 import config from './app/config/config';
+import RoutePath from './app/route/const/routePath';
 
 console.log('Starting User-API Dev Server...');
 
 const devServerPort: number = +config?.NODE_LOCAL_PORT;
 const environment: string = config?.NODE_ENV;
+const HealthRoutePath: string = RoutePath.HEALTH;
 
 if (devServerPort && environment) {
     App.listen(devServerPort, () => {
         console.log(`App environment: ${environment}`);
         console.log(`App is running on port: ${devServerPort}`);
+        console.log(`Check health: localhost:${devServerPort}${config.ROUTE_PATH}${HealthRoutePath}`);
     }).on('error', (error: Error) => {
         if (error) {
             const errorMessage = `An error occurred while starting the dev-server: 
--- server.ts
+++ server.ts
@@ -2,16 +2,19 @@
 
 import App from './app';
 import config from './app/config/config';
+import RoutePath from './app/route/const/routePath';
 
 console.log('Starting User-API Server...');
 
 const serverPort: number = +config?.NODE_LOCAL_PORT;
 const environment: string = config?.NODE_ENV;
+const HealthRoutePath: string = RoutePath.HEALTH;
 
 if (serverPort && environment) {
     App.listen(serverPort, () => {
         console.log(`App environment: ${environment}`);
         console.log(`App is running on port: ${serverPort}`);
+        console.log(`Check health: localhost:${serverPort}${config.ROUTE_PATH}${HealthRoutePath}`);
     }).on('error', (error: Error) => {
         if (error) {
             const errorMessage = `An error occurred while starting the server: 
