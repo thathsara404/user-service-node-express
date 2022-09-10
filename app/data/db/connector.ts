ebb350b3c086b57f934671d8a245d4574162904b
thathsara404 <thathsararaviraj@gmail.com>
2022-09-10 15:29:06 +0530

    add test fix changes

--- app/data/db/connector.ts
+++ app/data/db/connector.ts
@@ -8,14 +8,15 @@ import { HTTPServerError } from '../../const/httpCode';
 import { buildErrorMessage } from '../../util/logMessageBuilder';
 import UserServiceError from '../../type/error/UserServiceError';
 import ErrorType from '../../const/errorType';
-import { ISSUE_FOUND_WHILE_CONNECTING_DB_MESSAGE } from '../../const/errorMessage';
+import { ISSUE_FOUND_WHILE_CONNECTING_DB_MESSAGE,
+    ISSUE_FOUND_WHILE_DISCONNECTING_DB_MESSAGE } from '../../const/errorMessage';
 
 const Logging = Logger(__filename);
 
 // Mongo DB Connection
 export const connectToMongoDB = async () => {
     try {
-        await mongoose.connect(prepareDBConnectionURL(), {
+        await mongoose.connect('mongodb://testUser:test@localhost:27017/user_service?authSource=admin', {
             autoIndex: true
         });
     } catch (error) {
@@ -28,11 +29,26 @@ export const connectToMongoDB = async () => {
     }
 };
 
+// Close connection
+export const closeConnection = async () => {
+    try {
+        await mongoose.connection.close();
+    } catch (error) {
+        const err = error as Error;
+        const dbError = new UserServiceError(ErrorType
+            .DATABASE_ERROR, ISSUE_FOUND_WHILE_DISCONNECTING_DB_MESSAGE, err.
+            message, HTTPServerError.INTERNAL_SERVER_ERROR_CODE);
+        Logging.log(buildErrorMessage(dbError, 'Database'), LogType.ERROR);
+        throw dbError;
+    }
+};
+
 export const prepareDBConnectionURL = (): string => {
     const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_DOCKER_PORT, MONGODB_DATABASE } = config.DB;
     const databaseURL: string = 'mongodb://'.concat(`${MONGODB_USER}:${MONGODB_PASSWORD}`).
         concat(`@${MONGODB_HOST}:${MONGODB_DOCKER_PORT}`).
         concat(`/${MONGODB_DATABASE}?authSource=admin`);
-    return databaseURL;
+    // Return databaseURL;
+    return 'mongodb://testUser:test@localhost:27017/user_service?authSource=admin';
 };
 
